0f0648821089e8c371c0f4a4dd46298629b3aa1a
FILMMY <pratipran.s@mail.kmutt.ac.th>
2022-09-08 17:17:37 +0700

    Update 2

--- concatArray.js
+++ concatArray.js
@@ -5,10 +5,10 @@ function concatArray(array1, array2) {
     return 'underfined'
   }else if (array1 == [] || array2 == []){
     return "empty array"
-  }else if (array1 == null,undefined,[]){
-    return array2 
-  }else if(array2 == null,undefined,[]){
-    return array1
+  // }else if (array1 == null,undefined,[]){
+  //   return array2 
+  // }else if(array2 == null,undefined,[]){
+  //   return array1
   }else test = array1.concatArray(array2)
   
 
